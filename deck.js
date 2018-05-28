41eb746c43c73d5622f2028e281629196d803cb1
teropaj <teropaj@gmail.com>
2018-05-28 11:27:43 +0300

    made deck

--- deck.js
+++ deck.js
@@ -1,22 +1,55 @@
-var suites=['hearts','diamonds','clubs','spades'];
-var deckOfCards=[];
+ 
 
-function CardObject (suite,number){this.suite=suite;this.number=number;}
+class DeckOfCards {
+  constructor() {
+    this.suites=['hearts','diamonds','clubs','spades'];
+    this.deckOfCards=[];
+    this.cards=[]
+  }
+
+  CardObject (suite,number){this.suite=suite;this.number=number;}
 
 //makes a deck
-function deck() {
+  init() {
+    var valueOfCard;
+    var cardIndex=1;
+    for (let indexOfSuites in this.suites){
+      for(let index=1;index<=13;index++){
+        //cards(indexOfSuites,index);
+        //deck.suites[indexOfSuites]=index;
+        //console.log(indexOfSuites+' '+deck.indexOfSuites);
+        //console.log(index);
+        valueOfCard =(index==1) ? "ace1or11":(index>10) ? 10:index
+        //console.log(valueOfCard)
+        
 
 
-  for (indexOfSuites in suites){
-    for(index=1;index<=13;index++){
-    //cards(indexOfSuites,index);
-    //deck.suites[indexOfSuites]=index;
-    //console.log(indexOfSuites+' '+deck.indexOfSuites);
-    //console.log(index);
-    var card =new CardObject(suites[indexOfSuites],index);
-    deckOfCards.push(card);
 
+
+        this.cards[cardIndex] ={suite:this.suites[indexOfSuites],
+          number:index,value:valueOfCard};
+         
+        cardIndex++;
+
+      }
+     
+    }
+    this.shuffle();
+    console.log(this.cards);
+  //console.log('(makeDeck length is '+player.length+'counter is '+counter);
+  } 
+  giveCard (card){return this.cards[card];}
+
+  shuffle() {
+    for (var b=1;b<=52;b++){
+      var random=Math.floor(Math.random() * 52);
+      var c=this.cards[random];
+      
+      this.cards[random]=this.cards[b];
+      this.cards[b]=c;
+      
+      
     }
   }
-  //console.log('(makeDeck length is '+player.length+'counter is '+counter);
-}
+  get giveCards (){return this.cards;}
+} //class
